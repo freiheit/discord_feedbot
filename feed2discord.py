3cbe62033bf45a052511782a29e08732abded7be
Eric Eisenhart <freiheit@gmail.com>
2016-02-05 22:06:51 -0800

    Start the setup for multiple feeds

--- feed2discord.py
+++ feed2discord.py
@@ -32,11 +32,11 @@ for inifile in [os.path.expanduser('~')+'/.feed2discord.ini','feed2discord.ini']
 
 login_email = config['MAIN']['login_email']
 login_password = config['MAIN']['login_password']
-feed_url = config['MAIN']['feed_url']
-item_url_base = config['MAIN']['item_url_base']
 db_path = config['MAIN']['db_path']
-rss_refresh_time = config.getint('MAIN','rss_refresh_time')
-max_age = config.getint('MAIN','max_age')
+
+feeds = config.sections()
+feeds.remove('MAIN')
+feeds.remove('CHANNELS')
 
 # Crazy workaround for a bug with parsing that doesn't apply on all pythons:
 feedparser.PREFERRED_XML_PARSERS.remove('drv_libxml2')
@@ -49,11 +49,19 @@ conn.execute('''CREATE TABLE IF NOT EXISTS feed_items
 client = discord.Client()
 
 @asyncio.coroutine
-def background_check_feed():
+def background_check_feed(feed):
+    print('Starting up background_check_feed for '+feed)
     yield from client.wait_until_ready()
+
+    feed_url = config[feed]['feed_url']
+    item_url_base = config[feed]['item_url_base']
+    rss_refresh_time = config.getint(feed,'rss_refresh_time')
+    max_age = config.getint(feed,'max_age')
     channels = []
-    for key in config['CHANNELS']:
+    for key in config[feed]['channels'].split(','):
+        print(' adding '+key+' to '+feed)
         channels.append(discord.Object(id=config['CHANNELS'][key]))
+
     while not client.is_closed:
         print('fetching and parsing '+feed_url)
         feed_data = feedparser.parse(feed_url)
@@ -102,7 +110,8 @@ def on_ready():
 loop = asyncio.get_event_loop()
 
 try:
-    loop.create_task(background_check_feed())
+    for feed in feeds:
+        loop.create_task(background_check_feed(feed))
     loop.run_until_complete(client.login(login_email, login_password))
     loop.run_until_complete(client.connect())
 except Exception:
