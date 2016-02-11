b674bab99b9b48bcbd6bec67bfb58fe271f1fd65
Eric Eisenhart <freiheit@gmail.com>
2016-02-10 19:59:00 -0800

    Cleaning up the logger output, so it's easier to see which feed which output is from.

--- feed2discord.py
+++ feed2discord.py
@@ -62,7 +62,7 @@ client = discord.Client()
 
 @asyncio.coroutine
 def background_check_feed(feed):
-    logger.info('Starting up background_check_feed for '+feed)
+    logger.info(feed+': Starting up background_check_feed')
     yield from client.wait_until_ready()
     # make sure debug output has this check run in the right order...
     yield from asyncio.sleep(1)
@@ -75,79 +75,79 @@ def background_check_feed(feed):
     max_age = FEED.getint('max_age',86400)
     channels = []
     for key in FEED.get('channels').split(','):
-        logger.debug('adding channel '+key+' to '+feed)
+        logger.debug(feed+': adding channel '+key)
         channels.append(discord.Object(id=config['CHANNELS'][key]))
 
     while not client.is_closed:
         try:
-            logger.info('processing feed:'+feed)
+            logger.info(feed+': processing feed')
 
             http_headers = {}
 
+            ### Download the actual feed, if changed since last fetch
             cursor = conn.cursor()
             cursor.execute("select lastmodified,etag from feed_info where feed=? OR url=?",[feed,feed_url])
             data=cursor.fetchone()
             if data is None:
-                logger.info('feed is new to us? saving info')
+                logger.info(feed+':looks like updated version. saving info')
                 cursor.execute("REPLACE INTO feed_info (feed,url) VALUES (?,?)",[feed,feed_url])
                 conn.commit()
-                logger.debug('feed info saved')
+                logger.debug(feed+':feed info saved')
             else:
-                logger.debug('setting up extra headers for HTTP request.')
+                logger.debug(feed+':setting up extra headers for HTTP request.')
                 logger.debug(data)
                 lastmodified = data[0]
                 etag = data[1]
                 if lastmodified is not None and len(lastmodified):
-                    logger.debug('adding header If-Modified-Since: '+lastmodified)
+                    logger.debug(feed+':adding header If-Modified-Since: '+lastmodified)
                     http_headers['If-Modified-Since'] = lastmodified
                 else:
-                    logger.debug('no stored lastmodified')
+                    logger.debug(feed+':no stored lastmodified')
                 if etag is not None and len(etag):
-                    logger.debug('adding header ETag: '+etag)
+                    logger.debug(feed+':adding header ETag: '+etag)
                     http_headers['ETag'] = etag
                 else:
-                    logger.debug('no stored ETag')
-
-            logger.debug('sending http request for '+feed_url)
+                    logger.debug(feed+':no stored ETag')
+            logger.debug(feed+':sending http request for '+feed_url)
             http_response = yield from aiohttp.request('GET', feed_url, headers=http_headers)
             logger.debug(http_response)
-            logger.debug(http_response.headers)
             if http_response.status == 304:
-                logger.debug('data is old; moving on')
+                logger.debug(feed+':data is old; moving on')
                 raise HTTPNotModified()
             elif http_response.status != 200:
-                logger.debug('HTTP error: '+http_response.status)
+                logger.debug(feed+':HTTP error: '+http_response.status)
                 raise HTTPError()
             else:
-                logger.debug('HTTP success')
+                logger.debug(feed+':HTTP success')
 
-            logger.debug('reading http response')
+
+            logger.debug(feed+':reading http response')
             http_data = yield from http_response.read()
 
-            logger.debug('parsing http data')
+            logger.debug(feed+':parsing http data')
             feed_data = feedparser.parse(http_data)
-            logger.debug('done fetching')            
+            logger.debug(feed+':done fetching')            
 
 
             if 'ETAG' in http_response.headers:
                 etag = http_response.headers['ETAG']
-                logger.debug('saving etag: '+etag)
+                logger.debug(feed+':saving etag: '+etag)
                 cursor.execute("UPDATE feed_info SET etag=? where feed=? or url=?",[etag,feed,feed_url])
                 conn.commit()
-                logger.debug('etag saved')
+                logger.debug(feed+':etag saved')
             else:
-                logger.debug('no etag')
+                logger.debug(feed+':no etag')
 
             if 'LAST-MODIFIED' in http_response.headers:
                 modified = http_response.headers['LAST-MODIFIED']
-                logger.debug('saving lastmodified: '+modified)
+                logger.debug(feed+':saving lastmodified: '+modified)
                 cursor.execute("UPDATE feed_info SET lastmodified=? where feed=? or url=?",[modified,feed,feed_url])
                 conn.commit()
-                logger.debug('saved lastmodified')
+                logger.debug(feed+':saved lastmodified')
             else:
-                logger.debug('no last modified date')
+                logger.debug(feed+':no last modified date')
 
-            logger.debug('processing entries')
+            logger.debug(feed+':processing entries')
             for item in feed_data.entries:
                 id=item.id
                 pubDate=item.published
@@ -159,45 +159,45 @@ def background_check_feed(feed):
 
                 data=cursor.fetchone()
                 if data is None:
-                    logger.info('item '+id+' unseen, processing:')
+                    logger.info(feed+':item '+id+' unseen, processing:')
                     cursor.execute("INSERT INTO feed_items (id,published,title,url) VALUES (?,?,?,?)",[id,pubDate,title,url])
                     conn.commit()
                     if time.mktime(item.published_parsed) > (time.time() - max_age):
-                        logger.info(' fresh and ready for parsing')
+                        logger.info(feed+':item:fresh and ready for parsing')
                         description = re.sub('<br */>',"\n",original_description)
                         description = re.sub("\n+","\n",description)
                         if len(description) > 1800:
                           description = description[:1000] + "\n..."
-                        logger.debug(' published: '+pubDate)
-                        logger.debug(' title: '+title)
-                        logger.debug(' url: '+url)
+                        logger.debug(feed+':item:published: '+pubDate)
+                        logger.debug(feed+':item:title: '+title)
+                        logger.debug(feed+':item:url: '+url)
                         for channel in channels:
-                            logger.debug('sending message to '+channel.name()+' on '+channel.server())
+                            logger.debug(feed+':item:sending message to '+channel.name()+' on '+channel.server())
                             yield from client.send_message(channel,
                                url+"\n"+
                                "**"+title+"**\n"+
                                "*"+pubDate+"*\n"+
                                description)
                     else:
-                        logger.info(' too old; skipping')
+                        logger.info(feed+':too old; skipping')
                 else:
-                    logger.debug('item '+id+' seen before, skipping')
+                    logger.debug(feed+':item:'+id+' seen before, skipping')
         except HTTPNotModified:
-            logger.debug('Headers indicate feed unchanged since last time fetched: '+sys.exc_info()[0])
+            logger.debug(feed+':Headers indicate feed unchanged since last time fetched: '+sys.exc_info()[0])
         except HTTPError:
-            logger.debug('Unexpected HTTP error: '+sys.exc_info()[0])
-            logger.debug('Assuming error is transient and trying again later')
+            logger.debug(feed+':Unexpected HTTP error: '+sys.exc_info()[0])
+            logger.debug(feed+':Assuming error is transient and trying again later')
         except sqlite3.Error as sqlerr:
-            logger.debug('sqlite3 error: ')
+            logger.debug(feed+':sqlite3 error: ')
             logger.debug(sqlerr)
             raise
         except:
-            logger.debug('Unexpected error: '+sys.exc_info()[0])
-            logger.debug(feed,' is giving up')
+            logger.debug(feed+':Unexpected error: '+sys.exc_info()[0])
+            logger.debug(feed+':giving up')
             raise
         finally:
             # No matter what goes wrong, wait same time and try again
-            logger.debug('sleeping '+feed+' for '+str(rss_refresh_time)+' seconds')
+            logger.debug(feed+':sleeping for '+str(rss_refresh_time)+' seconds')
             yield from asyncio.sleep(rss_refresh_time)
         
 @client.async_event
